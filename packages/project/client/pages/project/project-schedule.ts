bc6919d4f5e56d2a3fa487c253e004a8f2dfae8c
Lee-YuMin <nalshya91@gmail.com>
2024-07-05 15:30:55 +0900

    fix: 간트차트 샘플 업로드

--- packages/project/client/pages/project/project-schedule.ts
+++ packages/project/client/pages/project/project-schedule.ts
@@ -14,6 +14,7 @@ import { notify } from '@operato/layout'
 
 import gql from 'graphql-tag'
 import { Building, Project } from './project-list'
+import '@operato/gantt/ox-gantt.js'
 
 @customElement('project-schedule')
 export class ProjectSchedule extends ScopedElementsMixin(PageView) {
@@ -95,6 +96,15 @@ export class ProjectSchedule extends ScopedElementsMixin(PageView) {
           div[chart] {
             flex: 0.7;
             background: #03a9f44d;
+
+            ox-gantt {
+              max-width: 91vw;
+              aspect-ratio: 3 / 1;
+              box-sizing: border-box;
+
+              background-color: var(--md-sys-color-primary-container);
+              color: var(--md-sys-color-on-primary-container);
+            }
           }
 
           div[table] {
@@ -156,6 +166,271 @@ export class ProjectSchedule extends ScopedElementsMixin(PageView) {
   @state() project: Project = { ...this.defaultProject }
   @state() selectedBuildingIdx: number = 0
 
+  private fromDate = '2022-12-27'
+  private toDate = '2024-12-31'
+  private timeScale = 'week-day'
+  private extendGridLines = false
+  private tasks = [
+    {
+      id: '1',
+      title: 'Design',
+      type: 'task',
+      // section: developmentSection,
+      startDate: '2023-01-01',
+      endDate: '2023-01-05',
+      progress: 60,
+      dependsOn: '',
+      length: '5d',
+      tags: ['active'],
+      children: []
+    },
+    {
+      id: '2',
+      title: 'Development',
+      type: 'milestone',
+      // section: developmentSection,
+      startDate: '2023-01-06',
+      endDate: '2023-01-06',
+      progress: 30,
+      dependsOn: '1',
+      length: '1d',
+      tags: ['milestone'],
+      children: []
+    },
+    {
+      id: '3',
+      title: 'Coding',
+      type: 'phase',
+      // section: developmentSection,
+      startDate: '2023-01-07',
+      endDate: '2023-01-15',
+      progress: 10,
+      dependsOn: '2',
+      length: '9d',
+      tags: ['critical'],
+      children: [
+        {
+          id: '3-1',
+          title: 'Coding-A',
+          type: 'task',
+          // section: developmentSection,
+          startDate: '2023-01-07',
+          endDate: '2023-01-10',
+          progress: 60,
+          dependsOn: '',
+          length: '4d',
+          tags: ['done'],
+          children: []
+        },
+        {
+          id: '3-2',
+          title: 'Coding-B',
+          type: 'task',
+          // section: developmentSection,
+          startDate: '2023-01-11',
+          endDate: '2023-01-15',
+          dependsOn: '3-1',
+          length: '5d',
+          tags: ['active'],
+          children: []
+        }
+      ]
+    },
+    {
+      id: '4',
+      title: 'Test',
+      type: 'milestone',
+      // section: developmentSection,
+      startDate: '2023-01-07',
+      endDate: '2023-01-10',
+      progress: 100,
+      dependsOn: '1',
+      length: '1d',
+      tags: ['done'],
+      children: []
+    },
+    {
+      id: '5',
+      title: 'Deploy',
+      type: 'milestone',
+      // section: developmentSection,
+      startDate: '2023-01-15',
+      endDate: '2023-01-21',
+      dependsOn: '4',
+      length: '1d',
+      tags: ['milestone'],
+      children: []
+    },
+    {
+      id: '6',
+      title: 'Publishing',
+      type: 'milestone',
+      // section: developmentSection,
+      startDate: '2023-01-11',
+      endDate: '2023-01-15',
+      dependsOn: '5',
+      length: '1d',
+      tags: ['milestone'],
+      children: []
+    },
+    {
+      id: '7',
+      title: 'Provisioning',
+      type: 'milestone',
+      // section: developmentSection,
+      startDate: '2023-01-12',
+      endDate: '2023-01-15',
+      dependsOn: '6',
+      length: '1d',
+      tags: ['milestone'],
+      children: []
+    },
+    {
+      id: '8',
+      title: 'Alpha Test',
+      type: 'milestone',
+      // section: testSection,
+      startDate: '2023-01-15',
+      endDate: '2023-01-17',
+      dependsOn: '7',
+      length: '1d',
+      tags: ['critical'],
+      children: []
+    },
+    {
+      id: '9',
+      title: 'Beta Task',
+      type: 'milestone',
+      // section: testSection,
+      startDate: '2023-01-15',
+      endDate: '2023-01-18',
+      dependsOn: '8',
+      length: '1d',
+      tags: ['active'],
+      children: []
+    },
+    {
+      id: '10',
+      title: 'Gamma Test',
+      type: 'milestone',
+      // section: testSection,
+      startDate: '2023-01-18',
+      endDate: '2023-01-19',
+      dependsOn: '9',
+      length: '1d',
+      tags: ['critical'],
+      children: []
+    },
+    {
+      id: '11',
+      title: 'Zeta Test',
+      type: 'milestone',
+      // section: testSection,
+      startDate: '2023-01-18',
+      endDate: '2023-01-22',
+      dependsOn: '9',
+      length: '1d',
+      tags: ['milestone'],
+      children: []
+    },
+    {
+      id: '12',
+      title: 'Release',
+      type: 'milestone',
+      // section: releaseSection,
+      startDate: '2023-01-18',
+      endDate: '2023-01-23',
+      dependsOn: '9',
+      length: '1d',
+      tags: ['milestone'],
+      children: []
+    },
+    {
+      id: '13',
+      title: 'Release',
+      type: 'milestone',
+      // section: releaseSection,
+      startDate: '2023-01-22',
+      endDate: '2023-01-27',
+      dependsOn: '12',
+      length: '1d',
+      tags: ['done'],
+      children: []
+    },
+    {
+      id: '14',
+      title: 'Release',
+      type: 'milestone',
+      // section: releaseSection,
+      startDate: '2023-01-27',
+      endDate: '2023-01-30',
+      dependsOn: '13',
+      length: '1d',
+      tags: ['milestone'],
+      children: []
+    },
+    {
+      id: '15',
+      title: 'Release',
+      type: 'milestone',
+      // section: releaseSection,
+      startDate: '2023-01-30',
+      endDate: '2023-01-31',
+      dependsOn: '14',
+      length: '1d',
+      tags: ['milestone'],
+      children: []
+    },
+    {
+      id: '16',
+      title: 'Release',
+      type: 'milestone',
+      // section: releaseSection,
+      startDate: '2023-01-31',
+      endDate: '2023-02-05',
+      dependsOn: '15',
+      length: '1d',
+      tags: ['active'],
+      children: []
+    },
+    {
+      id: '17',
+      title: 'Release',
+      type: 'milestone',
+      // section: releaseSection,
+      startDate: '2023-02-06',
+      endDate: '2023-02-07',
+      dependsOn: '16',
+      length: '1d',
+      tags: ['milestone'],
+      children: []
+    },
+    {
+      id: '18',
+      title: 'Release',
+      type: 'milestone',
+      // section: releaseSection,
+      startDate: '2023-02-08',
+      endDate: '2023-02-09',
+      dependsOn: '17',
+      length: '1d',
+      tags: ['critical'],
+      children: []
+    },
+    {
+      id: '19',
+      title: 'Release',
+      type: 'milestone',
+      // section: releaseSection,
+      startDate: '2023-02-10',
+      endDate: '2023-02-25',
+      dependsOn: '18',
+      length: '1d',
+      tags: ['milestone'],
+      children: []
+    }
+  ]
+
   render() {
     return html`
       <div header>
@@ -176,7 +451,20 @@ export class ProjectSchedule extends ScopedElementsMixin(PageView) {
       <div body>
         <div chart-container>
           <div chart>
-            <table></table>
+            <ox-gantt
+              from-date=${new Date(this.fromDate).toISOString().split('T')[0]}
+              to-date=${new Date(this.toDate).toISOString().split('T')[0]}
+              .timeScale=${this.timeScale}
+              .tasks=${this.tasks}
+              @date-range-selected=${(e: CustomEvent) => {
+                console.log('date-range-selected', e.detail)
+              }}
+              @task-clicked=${(e: CustomEvent) => {
+                console.log('task-clicked', e.detail)
+              }}
+              ?extend-grid-lines=${this.extendGridLines}
+            >
+            </ox-gantt>
           </div>
           <div table>
             <table></table>
